5bb1d305d052df433e77d16ef37f311810cb6fd6
Michael Petychakis <mpetyx@gmail.com>
2016-05-16 12:50:51 +0300

    major fixes

--- psy_questions/settings.py
+++ psy_questions/settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # URL prefix for static files.
 # psy_questions: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/app/staticfiles/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
