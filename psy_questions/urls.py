679807c8c8c0eec342241b360fea9e8472b5cd3f
Michael Petychakis <mpetyx@gmail.com>
2016-05-16 12:41:54 +0300

    one more try on static files

--- psy_questions/urls.py
+++ psy_questions/urls.py
@@ -18,5 +18,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
-              # + static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+)+ static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
