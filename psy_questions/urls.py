7041a7a57e692522d203975832086b5166b646fe
Michael Petychakis <mpetyx@gmail.com>
2016-05-16 12:35:44 +0300

    minor commit

--- psy_questions/urls.py
+++ psy_questions/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 admin.autodiscover()
 
@@ -15,4 +18,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+)+ static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
