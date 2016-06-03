6a64bad54339d999d97026b1dbd5f41c68cfa1d2
Michael Petychakis <mpetyx@gmail.com>
2016-06-03 11:07:58 +0300

    sending reminders every 30 minutes

--- email_campaigns/tasks.py
+++ email_campaigns/tasks.py
@@ -138,7 +138,7 @@ def start_campaign(emails):
 
 
 # @celery.decorators.periodic_task(run_every=crontab(day_of_month=[1,15]),ignore_result=True,name="task_check_who_filled_the_questionaire",)
-@celery.decorators.periodic_task(run_every=timedelta(seconds=30),ignore_result=True,name="task_check_who_filled_the_questionaire",)
+@celery.decorators.periodic_task(run_every=timedelta(minutes=30),ignore_result=True,name="task_check_who_filled_the_questionaire",)
 def check_who_filled_the_questionaire():
     for campaign in Campaign.objects.all():
         questionnaires = campaign.questionnaires.all()
