f55e6dfc61e152b6a1c6d1dc5c6a8ea0a0f8f392
Michael Petychakis <mpetyx@gmail.com>
2016-06-06 16:06:18 +0300

    major update on running multiple campaigns

--- email_campaigns/tasks.py
+++ email_campaigns/tasks.py
@@ -1,32 +1,25 @@
 __author__ = 'mpetyx'
 
+import celery
 from celery import shared_task
 from celery.utils.log import get_task_logger
-from celery.schedules import crontab
-import celery
+
 logger = get_task_logger(__name__)
-import datetime
-from questionnaire.models import RunInfoHistory, Subject, RunInfo
-from django.http import HttpResponse
 from django.template import Context
-from django.template.loader import render_to_string, get_template
+from django.template.loader import get_template
 from django.core.mail import EmailMessage
-from datetime import date
-import os
 from datetime import timedelta
 from .models import Campaign
 from questionnaire.views import *
 from django.contrib.sites.models import Site
 from django.template import loader
 from django.core.mail import send_mail
-
+from django.db.models.signals import post_save
 
 current_site = Site.objects.all()[0].name
 
 
-
-def generate_campaign_run( questionnaire_id, subject_id=None):
-
+def generate_campaign_run(questionnaire_id, subject_id=None):
     qu = get_object_or_404(Questionnaire, id=questionnaire_id)
     qs = qu.questionsets()[0]
 
@@ -52,23 +45,19 @@ def generate_campaign_run( questionnaire_id, subject_id=None):
 
     questionnaire_start.send(sender=None, runinfo=run, questionnaire=qu)
 
-    return str(current_site)+"/q/%s/"%key
-
-def retrieve_campaign_run( questionnaire_id, subject):
+    return str(current_site) + "/q/%s/" % key
 
 
-
+def retrieve_campaign_run(questionnaire_id, subject):
     qu = get_object_or_404(Questionnaire, id=questionnaire_id)
     qs = qu.questionsets()[0]
 
     if RunInfo.objects.filter(subject__email=subject, questionset=qs):
         run_key = RunInfo.objects.get(subject__email=subject, questionset=qs).runid
 
-        return str(current_site)+"/q/%s/"%run_key
+        return str(current_site) + "/q/%s/" % run_key
     else:
         return generate_campaign_run(questionnaire_id, subject)
-
-
 
 
 @shared_task(ignore_result=True)
@@ -77,15 +66,13 @@ def send_email_alert(email, questionnaire):
     to = [email]
     from_email = 'forgot_questionaires@apis.technology'
 
-
-
     ctx = {
         # 'user': str(user.first_name+ " "+user.last_name),
         # 'invoice_id': invoice_id,
         # 'amount' : amount,
         # 'overall_balance':balance,
         # 'billing_date':str(date.today())
-        'questionnaire':str(questionnaire)
+        'questionnaire': str(questionnaire)
     }
 
     message = get_template('mails/questionaire.html').render(Context(ctx))
@@ -97,13 +84,14 @@ def send_email_alert(email, questionnaire):
     html_message = loader.render_to_string(
         'mails/questionaire.html',
         {
-            'questionnaire':str(questionnaire)
-    }
+            'questionnaire': str(questionnaire)
+        }
     )
 
     send_mail(subject, message, from_email, to, fail_silently=True, html_message=html_message)
 
     return True
+
 
 @shared_task(ignore_result=True)
 def send_email_campagin(email, qu):
@@ -111,43 +99,66 @@ def send_email_campagin(email, qu):
     to = [email]
     from_email = 'welcome_questionaires@apis.technology'
 
-
-
     ctx = {
         # 'user': str(user.first_name+ " "+user.last_name),
         # 'invoice_id': invoice_id,
         # 'amount' : amount,
         # 'overall_balance':balance,
-        'qu':str(qu)
+        'qu': str(qu)
     }
 
     message = get_template('mails/welcome_questionaire.html').render(Context(ctx))
     msg = EmailMessage(subject, message, to=to, from_email=from_email)
-    msg.content_subtype = 'html'
-    msg.send()
+    # msg.content_subtype = 'html'
+    # msg.send()
+
+    html_message = loader.render_to_string(
+        'mails/welcome_questionaire.html',
+        {
+            'questionnaire': str(qu)
+        }
+    )
+
+    send_mail(subject, message, from_email, to, fail_silently=True, html_message=html_message)
 
     return True
 
+
 @shared_task(ignore_result=True)
 def start_campaign(emails):
-
     for email in emails:
         send_email_campagin.delay(email)
 
 
-
-
 # @celery.decorators.periodic_task(run_every=crontab(day_of_month=[1,15]),ignore_result=True,name="task_check_who_filled_the_questionaire",)
-@celery.decorators.periodic_task(run_every=timedelta(minutes=30),ignore_result=True,name="task_check_who_filled_the_questionaire",)
+@celery.decorators.periodic_task(run_every=timedelta(minutes=30), ignore_result=True,
+                                 name="task_check_who_filled_the_questionaire", )
 def check_who_filled_the_questionaire():
     for campaign in Campaign.objects.all():
         questionnaires = campaign.questionnaires.all()
         for questionnaire in questionnaires:
             emails = campaign.emails
             for email in emails:
-                if not RunInfoHistory.objects.filter(subject__email=email,questionnaire=questionnaire):
+                if not RunInfoHistory.objects.filter(subject__email=email, questionnaire=questionnaire):
+                    send_email_alert.delay(email, retrieve_campaign_run(questionnaire.id, email))
 
-                    send_email_alert.delay(email,retrieve_campaign_run(questionnaire.id,email))
 
-# from email_campaigns.tasks import send_email
-# send_email("mpetyx@me.com")
+def a_campaign_created(sender, instance, created, **kwargs):
+    a_campaign_created_celery.delay(instance)
+
+@shared_task(ignore_result=True)
+def a_campaign_created_celery(instance):
+    campaign = Campaign.objects.get(id=instance.id)
+
+    questionnaires = campaign.questionnaires.all()
+    for questionnaire in questionnaires:
+        emails = campaign.emails
+        for email in emails:
+            if not RunInfoHistory.objects.filter(subject__email=email, questionnaire=questionnaire):
+                if not RunInfo.objects.filter(subject__email=email, questionset=questionnaire.questionsets()[0]):
+                    send_email_campagin.delay(email, retrieve_campaign_run(questionnaire.id, email))
+
+
+post_save.connect(a_campaign_created, sender=Campaign)
+
+
