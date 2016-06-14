c3361071857fd974190f9487472691eb6580eae6
3nvi <agg.arvanitaks@gmail.com>
2016-06-14 13:45:40 +0300

    changed choices template

--- questionnaire/templatetags/app_filters.py
+++ questionnaire/templatetags/app_filters.py
@@ -10,3 +10,7 @@ register = template.Library()
 @register.filter
 def get_admin_results(cl):
     return cl.result_list
+
+@register.filter
+def qtext_parse(txt, part):
+    return txt.split('-')[int(part)].strip()
