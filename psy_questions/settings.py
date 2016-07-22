3d7bc10d91677bd2dc33b4bdb6a47e4c9f38dc92
Michael Petychakis <mpetyx@gmail.com>
2016-07-22 12:57:05 +0300

    ag production db

--- psy_questions/settings.py
+++ psy_questions/settings.py
@@ -13,22 +13,22 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# if os.environ.get('LOGNAME') in ['mpetyx', '3nvi']:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'psy_questions.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+if os.environ.get('LOGNAME') in ['mpetyx', '3nvi']:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'psy_questions.sqlite',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
-# else:
-#     import dj_database_url
-#
-#     DATABASES = dict()
-#     DATABASES['default'] = dj_database_url.config()
+else:
+    import dj_database_url
+
+    DATABASES = dict()
+    DATABASES['default'] = dj_database_url.config()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
