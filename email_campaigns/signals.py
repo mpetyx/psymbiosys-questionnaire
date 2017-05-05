a2d3c7b6fbdcc77ad2ae60c17f4bbf75725f6acd
3nvi <agg.arvanitaks@gmail.com>
2017-05-05 17:56:14 +0300

    added print logs

--- email_campaigns/signals.py
+++ email_campaigns/signals.py
@@ -5,6 +5,7 @@ from tasks import *
 
 def a_campaign_created(sender, instance, created, **kwargs):
     # if created:
+    print 'a campaign created'
     a_campaign_created_celery.delay(instance)
 
 post_save.connect(a_campaign_created, sender=Campaign)
