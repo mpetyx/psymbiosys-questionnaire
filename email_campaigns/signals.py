859f7db704cda79ef9811f265a2d1529b4ddc6ca
3nvi <agg.arvanitaks@gmail.com>
2017-05-05 16:44:23 +0300

    yolo

--- email_campaigns/signals.py
+++ email_campaigns/signals.py
@@ -4,7 +4,7 @@ from tasks import *
 
 
 def a_campaign_created(sender, instance, created, **kwargs):
-    if created:
-        a_campaign_created_celery.delay(instance)
+    # if created:
+    a_campaign_created_celery.delay(instance)
 
 post_save.connect(a_campaign_created, sender=Campaign)
