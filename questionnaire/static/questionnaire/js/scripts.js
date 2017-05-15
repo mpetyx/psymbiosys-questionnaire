bf270f623c5316ec1c67e0a158347673ef2ac56f
3nvi <agg.arvanitaks@gmail.com>
2017-05-15 16:19:32 +0300

    fixed a last bug

--- questionnaire/static/questionnaire/js/scripts.js
+++ questionnaire/static/questionnaire/js/scripts.js
@@ -365,7 +365,7 @@ $(document).ready(function() {
     
     $('select#table-campaign-filter').on('change', function() {
         var val = $(this).find('option:selected').data('val').toUpperCase(),
-            $table = $('table:not(.hidden)');
+            $table = $('table.analytics-table');
 
         $table
             .find('tr')
